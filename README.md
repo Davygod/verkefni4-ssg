47cdafada4f9415f80fb53a9e63945f06f2ccbb3
gjg <gjg@tskoli.is>
2020-03-18 17:39:07 +0000

    kóði yfirfarinn - GJG

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Verkefni4-ssg
-vefþ2vh
+
+# Python þróunarvefur
+
+Hér eru öll vinnugögnin - bætti við icomoon scss og skipti út grunnsíðunum (_templates_) þar sem það tekur alltof langan tíma að fara leiðrétta skrárnar.
+
+**Skoðaðu kóðann vel**  Í geymslunni **verefni4** eru (_output_) á gh-pages grein. 
+
+
--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from jinja2 import Environment, PackageLoader
 from markdown2 import markdown
 
-# hvar eru Markdown skjölin og skilgreining CSV gagna (yaml)
 POSTS = {}
 
 for markdown_post in os.listdir('content'):
@@ -13,22 +12,16 @@ for markdown_post in os.listdir('content'):
     with open(file_path, 'r') as file:
         POSTS[markdown_post] = markdown(file.read(), extras=['metadata'])
 
-# Uppröðun pósta eftir dagsetningu - nýjast efst
 POSTS = {
     post: POSTS[post] for post in sorted(POSTS, key=lambda post: datetime.strptime(POSTS[post].metadata['date'], '%Y-%m-%d'), reverse=True)
 }
 
-# Samsetning Markdown í Jinja template
-env = Environment(loader=PackageLoader('main', 'templates', 'Environment'))
-index_template = env.get_temmplate('index.html')
-home_template = env.get_template('bread.html')
+env = Environment(loader=PackageLoader('main', 'templates'))
+index_template = env.get_template('index.html')
+bread_template = env.get_template('bread.html')
 post_template = env.get_template('post.html')
 
-posts_metadata = [POSTS[post].metadata for post in POSTS]
-tags = [post['tags'] for post in posts_metadata]
-home_html = home_template.render(posts=posts_metadata, tags=tags)
-
-# forsíðan (er ekki með MD post rendiringu)
+# forsíðan er ekki með MD post renderingu
 index_html = index_template.render()
 
 # brauðuppskriftir
@@ -36,15 +29,12 @@ posts_metadata = [POSTS[post].metadata for post in POSTS]
 tags = [post['tags'] for post in posts_metadata]
 bread_html = bread_template.render(posts=posts_metadata, tags=tags)
 
-# Forsíðan með íslenskum stöfum
-with open('../recipes4/index.html', 'w', encoding='utf-8') as file:
+with open('../verkefni4/index.html', 'w', encoding='utf-8') as file:
     file.write(index_html)
 
-# brauðsíðan
-with open('../recipes4/bread.html', 'w') as file:
-    file.write(home_html)
+with open('../verkefni4/bread.html', 'w') as file:
+    file.write(bread_html)
 
-# Póstar - uppskriftir
 for post in POSTS:
     post_metadata = POSTS[post].metadata
 
@@ -54,10 +44,9 @@ for post in POSTS:
         'date': post_metadata['date'],
         'thumbnail': post_metadata['thumbnail']
     }
-    
-    # post_file_path - slóðin að uppfskriftunum
+
     post_html = post_template.render(post=post_data)
-    post_file_path = '../recipes4/posts/{slug}.html'.format(slug=post_metadata['slug'])
+    post_file_path = '../verkefni4/posts/{slug}.html'.format(slug=post_metadata['slug'])
 
     os.makedirs(os.path.dirname(post_file_path), exist_ok=True)
     with open(post_file_path, 'w') as file:
